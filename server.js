6f2574a2af02f8e042d9148840ae6af4b3e6f293
Chris Klanac <cklanac@gmail.com>
2017-03-30 06:30:31 -0400

    Solves Test 1
    Create .get endpoint, add CORS header and remove express static public.

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  res.json({});
+});
 
 app.listen(process.env.PORT || 8080);
 
