f20b9cf794298b9e9e4737a50d82f7a386a74f9a
Chris Klanac <cklanac@gmail.com>
2017-03-30 06:32:45 -0400

    Solve Test 1 - Part 2
    Install cors module and add to express middleware

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express');
+var cors = require('cors');
 
 var app = express();
 
 // app.use(express.static('public'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// Use cors module or set headers manually
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/', function(req, res, next) {
   res.json({});
